3ac352eb5f04e045017e702c58080392991f16d1
greenn <greenn.lab@gmail.com>
2021-11-09 09:17:48 +0900

    init

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "arrowParens": "always",
+  "bracketSameLine": false,
+  "bracketSpacing": true,
+  "embeddedLanguageFormatting": "auto",
+  "htmlWhitespaceSensitivity": "css",
+  "printWidth": 80,
+  "semi": true,
+  "singleQuote": false,
+  "trailingComma": "none",
+  "vueIndentScriptAndStyle": false
+}
